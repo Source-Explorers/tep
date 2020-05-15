615b12941dc872f211b2b8675e5e68423ea0f505
Nils D. Döring <mail@nils-doering.de>
2020-05-15 22:35:30 +0200

    Add first default paths for configuration files
    This commit add a server, that should provide a access to the
    configuration for TEP. Currently the configuration_server only searches
    for the configuration file. It therefore searches the default
    directories and gives access to the first found file.
    
    Further development should include the old-style config file directory
    on linux systems, normally located in ~/.tep/config.ini .

---
+++ include/configuration_server.hrl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. May 2020 9:54 PM
+%%%-------------------------------------------------------------------
+
+%% @doc This macro defines the possible file names for the configuration files.
+-define(FILE_NAMES, [
+    "config.ini"
+  ]).
+
+%% @doc This macro defines the directories in which the config file is searched.
+-define(CONFIG_PATHS, [
+    filename:absname(""),
+    filename:basedir(user_config, "tep/"),
+    "/etc/tep/"
+  ]).
