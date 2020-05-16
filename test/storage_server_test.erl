6e83a1343e49bec3aecaae48d6f7c3bda28702f4
Nils Koch <nkoch@seibert-media.net>
2020-05-16 16:47:53 +0200

    Reformat new changes from develop

--- test/storage_server_test.erl
+++ test/storage_server_test.erl
@@ -9,15 +9,16 @@
 -include("storage_server.hrl").
 
 storage_test_() ->
-	TempDir = string:strip(os:cmd("mktemp -d"), right, $\n),
-	{
-		setup,
-		fun() -> TempDir end,
-		fun(Dir) -> os:cmd("rm -r " ++ Dir) end,
-		{with, TempDir, [fun storage_init/1]}
-	}.
+    TempDir = string:strip(os:cmd("mktemp -d"), right, $\n),
+    {
+        setup,
+        fun () -> TempDir end,
+        fun (Dir) -> os:cmd("rm -r " ++ Dir) end,
+        {with, TempDir, [fun storage_init/1]}
+    }.
 
 storage_init(TempDir) ->
-	FileName = filename:absname_join(TempDir, "config.dets"),
-	{ok, #storage_server_state{path = TempDir, config = config}} = storage_server:init(TempDir),
-	?assert(dets:is_dets_file(FileName)).
+    FileName = filename:absname_join(TempDir, "config.dets"),
+    {ok, #storage_server_state{path = TempDir, config = config}} =
+        storage_server:init(TempDir),
+    ?assert(dets:is_dets_file(FileName)).
