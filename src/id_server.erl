2d01f4f495867037d84b2d31dce96a837b3a0ed1
Nils D. Döring <mail@nils-doering.de>
2020-05-16 14:06:00 +0200

    Reformating the code with erlfmt

--- src/id_server.erl
+++ src/id_server.erl
@@ -6,7 +6,14 @@
 -export([start_link/0]).
 
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([
+    init/1,
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2,
+    terminate/2,
+    code_change/3
+]).
 
 %% business logic API
 -export([get_id/1, shutdown/1]).
