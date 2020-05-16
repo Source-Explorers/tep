9457c795ea09455064970aba509bcb5a75a4198f
Nils D. Döring <mail@nils-doering.de>
2020-05-16 22:28:29 +0200

    Add command-line flag -config_file
    To specify a config file at startup, this adds a routine to check for a
    command-line flag. The flag, which is used is config_flag which is to be
    followed by a string, that designates the config file. If multiple of
    these flags a given, only the first is used. If no flag is specified,
    the atom no_path is used. The string is given to tep_sup:start_link/1
    for further use.

--- src/tep_app.erl
+++ src/tep_app.erl
@@ -8,8 +8,15 @@
 
 -export([start/2, stop/1]).
 
-start(_StartType, _StartArgs) -> tep_sup:start_link().
+start(_StartType, _StartArgs) -> tep_sup:start_link(get_argument_string(config_file)).
 
 stop(_State) -> ok.
 
 %% internal functions
+-spec get_argument_string(Name :: atom()) ->
+    string().
+get_argument_string(Name) ->
+    case init:get_argument(Name) of
+        {ok, [[Value] | _]} -> Value;
+        _ -> no_path
+    end.
--- src/tep_sup.erl
+++ src/tep_sup.erl
@@ -6,14 +6,14 @@
 
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/1]).
 
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
 
-start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+start_link(ConfigPath) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [ConfigPath]).
 
 %% sup_flags() = #{strategy => strategy(),         % optional
 %%                 intensity => non_neg_integer(), % optional
@@ -24,7 +24,7 @@ start_link() ->
 %%                  shutdown => shutdown(), % optional
 %%                  type => worker(),       % optional
 %%                  modules => modules()}   % optional
-init([]) ->
+init([_ConfigPath]) ->
     SupFlags = #{strategy => one_for_all, intensity => 0, period => 1},
     Storage = #{
         id => storage_sup,
