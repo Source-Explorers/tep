278cbe5346fe20f6fdd3d947c94f4659bca6892b
Nils D. Döring <mail@nils-doering.de>
2020-05-19 20:48:06 +0200

    Simple mocking for the config_file_name() function

--- test/configuration_server_test.erl
+++ test/configuration_server_test.erl
@@ -1,9 +1,3 @@
-%%%-------------------------------------------------------------------
-%%% @doc
-%%%
-%%% @end
-%%% Created : 16. May 2020 8:04 PM
-%%%-------------------------------------------------------------------
 -module(configuration_server_test).
 
 -include_lib("eunit/include/eunit.hrl").
@@ -14,11 +8,17 @@ configuration_server_test_() ->
     [
         ?_assertEqual(?CONFIG_PATHS, configuration_server:default_directories()),
         ?_assertEqual(
-            {error, {no_file, "No config file found"}},
-            configuration_server:config_file_name()
-        ),
-        ?_assertEqual(
             {stop, {no_file, "No config file found"}},
             configuration_server:init([])
         )
     ].
+
+config_file_name_test() ->
+    meck:new(filelib, [unstick]),
+    meck:expect(filelib, is_regular, fun(_Path) -> true end),
+    ?assertEqual(
+        {ok, filename:absname("config.ini")},
+        configuration_server:config_file_name()
+    ),
+    ?assert(meck:validate(filelib)),
+    meck:unload().
