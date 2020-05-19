ff450f57d07facb93aaee793bb5e4dea8be7c883
Nils D. Döring <mail@nils-doering.de>
2020-05-19 21:02:08 +0200

    Add mocking for configuration_server:init/1
    To test the behaviour of the configuration_server:init/1 function, this
    mocking implementation tests the values that should be returned if no
    configuration file could be found and if multiple are found.

--- test/configuration_server_test.erl
+++ test/configuration_server_test.erl
@@ -4,21 +4,29 @@
 
 -include("configuration_server.hrl").
 
-configuration_server_test_() ->
-    [
-        ?_assertEqual(?CONFIG_PATHS, configuration_server:default_directories()),
-        ?_assertEqual(
-            {stop, {no_file, "No config file found"}},
-            configuration_server:init([])
-        )
-    ].
+default_directories_test() ->
+    ?assertEqual(?CONFIG_PATHS, configuration_server:default_directories()).
 
 config_file_name_test() ->
     meck:new(filelib, [unstick]),
-    meck:expect(filelib, is_regular, fun(_Path) -> true end),
+    meck:expect(filelib, is_regular, fun (_Path) -> true end),
     ?assertEqual(
         {ok, filename:absname("config.ini")},
         configuration_server:config_file_name()
     ),
     ?assert(meck:validate(filelib)),
     meck:unload().
+
+init_test() ->
+    meck:new(filelib, [unstick]),
+    meck:expect(filelib, is_regular, fun (_Path) -> true end),
+    ?assertEqual(
+        {ok,{configuration,filename:absname("config.ini")}},
+        configuration_server:init([])
+    ),
+    ?assert(meck:validate(filelib)),
+    meck:unload(),
+    ?assertEqual(
+        {stop, {no_file, "No config file found"}},
+        configuration_server:init([])
+    ).
