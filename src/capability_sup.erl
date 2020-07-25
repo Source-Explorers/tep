b64375b3aa0b52bf9247c3c314583452dba683f9
CptCaptain <nilskoch@posteo.de>
2020-07-25 15:05:19 +0200

    Add capability server as child

--- src/capability_sup.erl
+++ src/capability_sup.erl
@@ -13,38 +13,42 @@
 %%%===================================================================
 %%% API functions
 %%%===================================================================
-
 %% @doc Starts the supervisor
--spec(start_link() -> {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+-spec start_link() -> {ok, Pid :: pid()} | ignore | {error, Reason :: term()}.
 start_link() ->
-  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 %%%===================================================================
 %%% Supervisor callbacks
 %%%===================================================================
-
 %% @private
 %% @doc Whenever a supervisor is started using supervisor:start_link/[2,3],
 %% this function is called by the new process to find out about
 %% restart strategy, maximum restart frequency and child
 %% specifications.
 -spec init(Args :: term()) ->
-  {ok, {SupFlags :: supervisor:sup_flags(), [ChildSpec :: supervisor:child_spec()]}} |
-  ignore.
+    {ok, {SupFlags :: supervisor:sup_flags(), [ChildSpec :: supervisor:child_spec()]}} |
+    ignore.
 init([]) ->
-  MaxRestarts = 10,
-  MaxSecondsBetweenRestarts = 1,
-  SupFlags = #{strategy => one_for_one,
-    intensity => MaxRestarts,
-    period => MaxSecondsBetweenRestarts},
+    MaxRestarts = 10,
+    MaxSecondsBetweenRestarts = 1,
+    SupFlags = #{
+        strategy => one_for_one,
+        intensity => MaxRestarts,
+        period => MaxSecondsBetweenRestarts
+    },
 
-  Date = #{
-    id => date,
-    start =>
-    {sensor_server, start_link, [date, fun () -> calendar:local_time() end, 0]}
-  },
+    CapServer = #{
+        id => starter,
+        start => {capability_server, start_link, []}
+    },
 
-  {ok, {SupFlags, [Date]}}.
+    Date = #{
+        id => date,
+        start => {sensor_server, start_link, [date, fun () -> calendar:local_time() end, 0]}
+    },
+
+    {ok, {SupFlags, [CapServer, Date]}}.
 
 %%%===================================================================
 %%% Internal functions
